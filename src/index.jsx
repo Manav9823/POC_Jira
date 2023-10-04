b34d77f365a887760d82594204d10c8a1b5f8ae8
Manav9823 <manavbansalfddk@gmail.com>
2023-10-04 16:09:01 +0530

    'POC for jira app'

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from '@forge/ui';
+import api, { route } from "@forge/api";
+
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
+
+  const data = await res.json();
+  return data.comments;
+};  
+
+const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+
+console.log(`Number of comments on this issue: ${comments.length}`);
+  return (
+    <Fragment>
+      <Text>Hello world!</Text>
+      <Text>
+        Number of comments on this issue: {comments.length}
+      </Text>
+      <Text>Hello from manav and we go again</Text>
+    </Fragment>
+  );
+};
+
+export const run = render(
+  <IssuePanel>
+    <App />
+  </IssuePanel>
+);
